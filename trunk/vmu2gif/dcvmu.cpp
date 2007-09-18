fe77dc14feb0a93b87ef6f560086d40fc4f628c9
bucanero <bucanero@4ac9e7bb-4771-46d1-b468-ef26d6a1d0ed>
2007-09-18 23:41:43 +0000

    vmu2gif v1.0 source code

---
+++ trunk/vmu2gif/dcvmu.cpp
@@ -0,0 +1,456 @@
+/********************************************************************
+ * Dreamcast VMU File Handling foundation classes v0.2 (Dec/2005)
+ * dcvmu.cpp - coded by El Bucanero
+ *
+ * Copyright (C) 2005 Damian Parrino <bucanero@fibertel.com.ar>
+ * http://www.bucanero.com.ar/
+ *
+ * Greetz to:
+ * Marcus Comstedt for his great info & resources					<http://mc.pp.se/dc/>
+ * Dan Potter for KallistiOS and other great libs like Tsunami		<http://gamedev.allusion.net/>
+ * AndrewK / Napalm 2001 for the lovely DC load-ip/tool				<http://adk.napalm-x.com/>
+ * Lawrence Sebald for the MinGW/msys cross compiler tutorial		<http://ljsdcdev.sunsite.dk/>
+ *
+ * and last, but not least, thanks to SEGA for my ALWAYS LOVED Dreamcast! =)
+ *
+ ********************************************************************/
+
+#include "dcvmu.hpp"
+
+#include <string.h>
+#include <stdio.h>
+
+//--------------------
+//	Class DreamcastFile
+//--------------------
+
+DreamcastFile::DreamcastFile() {
+	strcpy(name, "");
+	strcpy(path, "");
+	size=0;
+	data=NULL;
+	game=false;
+	copyProtect=false;
+	memset(&time, 0x00, 0x08);
+}
+
+DreamcastFile::~DreamcastFile() {
+	if(data != NULL) {
+		delete data;
+	}
+}
+
+void DreamcastFile::setName(char *n) {
+//	pName=new char[strlen(name)];
+	strncpy(name, n, 12);
+}
+
+char* DreamcastFile::getName() {
+	return(name);
+};
+
+void DreamcastFile::setSize(ssize_t s) {
+	size=s;
+};
+
+ssize_t DreamcastFile::getSize() {
+	return(size);
+};
+
+void DreamcastFile::setTime(vmu_timestamp_t t) {
+	time=t;
+};
+
+vmu_timestamp_t DreamcastFile::getTime() {
+	return(time);
+};
+
+void DreamcastFile::timeToBCD(vmu_timestamp_t *tdec) {
+	tdec->cent=dec_to_bcd(tdec->cent);
+	tdec->year=dec_to_bcd(tdec->year);
+	tdec->month=dec_to_bcd(tdec->month);
+	tdec->day=dec_to_bcd(tdec->day);
+	tdec->hour=dec_to_bcd(tdec->hour);
+	tdec->min=dec_to_bcd(tdec->min);
+	tdec->sec=dec_to_bcd(tdec->sec);
+	tdec->dow=dec_to_bcd(tdec->dow);
+}
+
+void DreamcastFile::timeToDec(vmu_timestamp_t *tbcd) {
+	tbcd->cent=bcd_to_dec(tbcd->cent);
+	tbcd->year=bcd_to_dec(tbcd->year);
+	tbcd->month=bcd_to_dec(tbcd->month);
+	tbcd->day=bcd_to_dec(tbcd->day);
+	tbcd->hour=bcd_to_dec(tbcd->hour);
+	tbcd->min=bcd_to_dec(tbcd->min);
+	tbcd->sec=bcd_to_dec(tbcd->sec);
+	tbcd->dow=bcd_to_dec(tbcd->dow);
+}
+
+void DreamcastFile::setData(uint8 *d) {
+/*	if(data != NULL) {
+		delete data;
+	}
+	data=new uint8[size];
+	memcpy(data, d, size);
+*/
+	data=d;
+}
+
+uint8* DreamcastFile::getData() {
+	return(data);
+}
+
+void DreamcastFile::setGameFile(bool t) {
+	game=t;
+}
+
+bool DreamcastFile::isGameFile() {
+	return(game);
+}
+
+void DreamcastFile::setCopyProtected(bool cp) {
+	copyProtect=cp;
+}
+
+bool DreamcastFile::isCopyProtected() {
+	return(copyProtect);
+}
+
+void DreamcastFile::setPath(char *p) {
+	strncpy(path, p, 63);
+}
+
+char* DreamcastFile::getPath() {
+	return(path);
+};
+
+uint8 DreamcastFile::dec_to_bcd(uint8 dec) {
+	uint8 rv = 0;
+
+	rv = dec % 10;
+	rv |= ((dec / 10) % 10) << 4;
+	return rv;
+}
+
+uint8 DreamcastFile::bcd_to_dec(uint8 bcd) {
+	uint8 rv = 0;
+
+	rv=0x0f & bcd;
+	rv+=((0xf0 & bcd) >> 4) * 10;
+	return rv;
+}
+
+int DreamcastFile::readFile() {
+//vmufs_write(*maple, name, data, size, flags)
+	return(0);
+}
+
+int DreamcastFile::writeFile() {
+//vmufs_write(*maple, name, data, size, flags)
+	return(0);
+}
+
+uint16* DreamcastFile::getIconPalette() {
+	if (game) {
+		return((uint16*)(data+0x260));
+	} else {
+		return((uint16*)(data+0x60));
+	}
+}
+
+uint8* DreamcastFile::getIconBitmap(uint8 i) {
+	if (game) {
+		return(data+0x280+(i*512));
+	} else {
+		return(data+0x80+(i*512));
+	}
+}
+
+uint8 DreamcastFile::getIcons() {
+	if (game) {
+		return(*(data+0x240));
+	} else {
+		return(*(data+0x40));
+	}
+}
+
+uint8 DreamcastFile::getAnimationSpeed() {
+	return(*(data+0x42));
+}
+
+void DreamcastFile::loadHeader(vmu_dir_t *header) {
+	game=(header->filetype == 0xCC);
+	copyProtect=(header->copyprotect == 0xFF);
+	setName(header->filename);
+	time=header->timestamp;
+	size=header->filesize*512;
+}
+
+void DreamcastFile::buildHeader(vmu_dir_t *header) {
+	memset(header, 0x00, 0x20);
+	header->filetype=game ? 0xCC : 0x33;
+	header->copyprotect=copyProtect ? 0xFF : 0x00;
+	strcpy(header->filename, name);
+	header->timestamp=time;
+	header->filesize=size/512;
+}
+
+//--------------------
+//	Class VirtualFile
+//--------------------
+
+VirtualFile::VirtualFile() {
+	dcf=NULL;
+	strcpy(filename, "");
+}
+
+VirtualFile::~VirtualFile() {
+	if(dcf != NULL) {
+		delete dcf;
+	}
+}
+
+void VirtualFile::setFileName(char *fn) {
+	strcpy(filename, fn);
+}
+
+char* VirtualFile::getFileName() {
+	return(filename);
+};
+
+DreamcastFile* VirtualFile::getDCFile() {
+	return(dcf);
+};
+
+void VirtualFile::setDCFile(DreamcastFile *df) {
+	dcf=df;
+};
+
+int VirtualFile::readFile(char *fn) {
+	setFileName(fn);
+	return(readData());
+};
+
+int VirtualFile::writeFile(char *fn) {
+	setFileName(fn);
+	return(writeData());
+};
+
+//--------------------
+//	Class DCIFile
+//--------------------
+
+DCIFile::DCIFile() {
+}
+
+DCIFile::DCIFile(char *fn) {
+	setFileName(fn);
+}
+
+DCIFile::~DCIFile() {
+//	this->VirtualFile::~VirtualFile();
+}
+
+int DCIFile::readData() {
+	file_t f;
+	uint8 *raw;
+	vmu_dir_t header;
+	DreamcastFile *df;
+	
+	df=getDCFile();
+	if(df == NULL) {
+		df=new DreamcastFile();
+		setDCFile(df);
+	}
+	if (!(f=fs_open(getFileName(), O_RDONLY))) {
+		printf("ERROR: Can't open %s!\n", getFileName());
+		return(-1);
+	}
+	fs_read(f, &header, 0x20);
+	df->loadHeader(&header);
+//	raw=(uint8*)malloc(df->getSize());
+	raw=new uint8[df->getSize()];
+	fs_read(f, raw, df->getSize());
+	df->setData(swapData(raw));
+//	free(raw);
+	fs_close(f);
+	return(0);
+}
+
+int DCIFile::writeData() {
+	file_t f;
+	vmu_dir_t header;
+	DreamcastFile *df;
+
+	df=getDCFile();
+	if (!(f=fs_open(getFileName(), "wb"))) {
+		printf("ERROR: Can't open %s!\n", getFileName());
+		return(-1);
+	}
+	df->buildHeader(&header);
+	fs_write(f, &header, 0x20);
+	fs_write(f, swapData(df->getData()), df->getSize());
+	fs_close(f);
+	swapData(df->getData());
+	return(0);
+}
+
+uint8* DCIFile::swapData(uint8 *raw) {
+	uint8 tmp[4];
+
+	for (int i=0; i<getDCFile()->getSize(); i+=4) {
+		tmp[3]=raw[i];
+		tmp[2]=raw[i+1];
+		tmp[1]=raw[i+2];
+		tmp[0]=raw[i+3];
+		memcpy(raw+i, tmp, 4);
+	}
+	return(raw);
+}
+
+//--------------------
+//	Class VMIFile
+//--------------------
+
+VMIFile::VMIFile() {
+	strcpy(description, "");
+	strcpy(copyright, "");
+	strcpy(resource_name, "");
+}
+
+VMIFile::VMIFile(char *fn, char *ds, char *cr, char *rn) {
+	setFileName(fn);
+	setDescription(ds);
+	setCopyright(cr);
+	setResourceName(rn);
+}
+
+VMIFile::~VMIFile() {
+//	this->VirtualFile::~VirtualFile();
+}
+
+void VMIFile::setResourceName(char *rn) {
+	strncpy(resource_name, rn, 8);
+}
+
+char* VMIFile::getResourceName() {
+	return(resource_name);
+};
+
+void VMIFile::setCopyright(char *c) {
+	strncpy(copyright, c, 32);
+}
+
+char* VMIFile::getCopyright() {
+	return(copyright);
+};
+
+void VMIFile::setDescription(char *d) {
+	strncpy(description, d, 32);
+}
+
+char* VMIFile::getDescription() {
+	return(description);
+};
+
+void VMIFile::loadHeader(vmi_hdr_t *header) {
+	DreamcastFile *df;
+	uint16 year;
+
+	df=getDCFile();
+	setDescription(header->description);
+	setCopyright(header->copyright);
+	setResourceName(header->resource_name);
+	df->setSize(header->filesize);
+	df->setName(header->filename);
+	df->setGameFile(header->filemode & VMI_VMUGAME);
+	df->setCopyProtected(header->filemode & VMI_NOCOPY);
+	memcpy(&year, &header->timestamp.cent, 2);
+	header->timestamp.cent=year / 100;
+	header->timestamp.year=year % 100;
+	header->timestamp.dow=(header->timestamp.dow == 0) ? 6 : header->timestamp.dow-1;
+	df->timeToBCD(&header->timestamp);
+	df->setTime(header->timestamp);
+}
+
+int VMIFile::readData() {
+	file_t f;
+	vmi_hdr_t header;
+	uint8 *raw;
+	char tmp[13];
+	DreamcastFile *df;
+	
+	df=getDCFile();
+	if(df == NULL) {
+		df=new DreamcastFile();
+		setDCFile(df);
+	}
+	if (!(f=fs_open(getFileName(), O_RDONLY))) {
+		printf("ERROR: Can't open %s!\n", getFileName());
+		return(-1);
+	}
+	fs_read(f, &header, 0x6C);
+	fs_close(f);
+	loadHeader(&header);
+	sprintf(tmp, "%s.VMS", getResourceName());
+	if (!(f=fs_open(tmp, O_RDONLY))) {
+		printf("ERROR: Can't open %s!\n", tmp);
+		return(-2);
+	}
+//	raw=(uint8*)malloc(df->getSize());
+	raw=new uint8[df->getSize()];
+	fs_read(f, raw, df->getSize());
+	df->setData(raw);
+//	free(raw);
+	fs_close(f);
+	return(0);
+}
+
+void VMIFile::buildHeader(vmi_hdr_t *header) {
+	DreamcastFile *df;
+	uint16 year;
+	
+	df=getDCFile();
+	memset(header, 0x00, 0x6C);
+	strcpy(header->description, getDescription());
+	strcpy(header->copyright, getCopyright());
+	strcpy(header->resource_name, getResourceName());
+	header->checksum[0]=header->resource_name[0] & 'S';
+	header->checksum[1]=header->resource_name[1] & 'E';
+	header->checksum[2]=header->resource_name[2] & 'G';
+	header->checksum[3]=header->resource_name[3] & 'A';
+	header->timestamp=df->getTime();
+	df->timeToDec(&header->timestamp);
+	year=header->timestamp.cent*100 + header->timestamp.year;
+	memcpy(&header->timestamp.cent, &year, 2);
+	header->timestamp.dow=(header->timestamp.dow == 6) ? 0 : header->timestamp.dow+1;
+	header->filenumber=0x01;
+	strcpy(header->filename, df->getName());
+	header->filemode=((df->isGameFile()) ? VMI_VMUGAME : 0x00) | ((df->isCopyProtected()) ? VMI_NOCOPY : 0x00);
+	header->filesize=df->getSize();
+}
+
+int VMIFile::writeData() {
+	file_t f;
+	vmi_hdr_t header;
+	DreamcastFile *df;
+	char tmp[13];
+	
+	df=getDCFile();
+	if (!(f=fs_open(getFileName(), "wb"))) {
+		printf("ERROR: Can't open %s!\n", getFileName());
+		return(-1);
+	}
+	buildHeader(&header);
+	fs_write(f, &header, 0x6C);
+	fs_close(f);
+	sprintf(tmp, "%s.VMS", getResourceName());
+	if (!(f=fs_open(tmp, "wb"))) {
+		printf("ERROR: Can't open %s!\n", tmp);
+		return(-2);
+	}
+	fs_write(f, df->getData(), df->getSize());
+	fs_close(f);
+	return(0);
+}
---
+++ trunk/vmu2gif/vmu2gif.cpp
@@ -0,0 +1,150 @@
+/********************************************************************
+ * VMU 2 GIF v1.0 (Dec/2005)
+ * vmu2gif.cpp - coded by El Bucanero
+ *
+ * Copyright (C) 2005 Damian Parrino <bucanero@fibertel.com.ar>
+ * http://www.bucanero.com.ar/
+ *
+ * Greetz to:
+ * Marcus Comstedt for his great info & resources					<http://mc.pp.se/dc/>
+ * Dan Potter for KallistiOS and other great libs like Tsunami		<http://gamedev.allusion.net/>
+ * AndrewK / Napalm 2001 for the lovely DC load-ip/tool				<http://adk.napalm-x.com/>
+ * Lawrence Sebald for the MinGW/msys cross compiler tutorial		<http://ljsdcdev.sunsite.dk/>
+ * Sverre H. Huseby for the GIF save routines					<sverrehu@ifi.uio.no>
+ *
+ * and last, but not least, thanks to SEGA for my ALWAYS LOVED Dreamcast! =)
+ *
+ ********************************************************************/
+
+#include <string.h>
+#include <stdio.h>
+
+#include "gifsave.hpp"
+#include "dcvmu.hpp"
+
+//--------------------
+//	Main functions
+//--------------------
+
+char* decodeMonth(uint8 m) {
+	switch(m) {
+		case 0x01: return("Jan");
+		case 0x02: return("Feb");
+		case 0x03: return("Mar");
+		case 0x04: return("Apr");
+		case 0x05: return("May");
+		case 0x06: return("Jun");
+		case 0x07: return("Jul");
+		case 0x08: return("Aug");
+		case 0x09: return("Sep");
+		case 0x10: return("Oct");
+		case 0x11: return("Nov");
+		default: return("Dec");
+	}
+}
+
+void printFileInfo(DreamcastFile *df) {
+	vmu_timestamp_t ts;
+	
+	ts=df->getTime();
+	printf(" [i] Name: %s\n", df->getName());
+	printf(" [i] Size: %d bytes\n", df->getSize());
+	printf(" [i] Type: %s File\n", ((df->isGameFile()) ? "Game" : "Data"));
+	printf(" [i] Date: %02x %s %02x%02x\n", ts.day, decodeMonth(ts.month), ts.cent, ts.year);
+	printf(" [i] Time: %02x:%02x:%02x\n", ts.hour, ts.min, ts.sec);
+	printf(" [i] Copy Protected: %s\n", ((df->isCopyProtected()) ? "Yes" : "No"));
+}
+
+int writeIconGif(char *fn, VirtualFile *src) {
+	uint8 hdr[13]={0x47, 0x49, 0x46, 0x38, 0x39, 0x61, 0x20, 0x00, 0x20, 0x00, 0xB3, 0x00, 0x00};
+	uint8 ani[19]={0x21, 0xF9, 0x04, 0x00, 0x33, 0x00, 0x07, 0x00, 0x2C, 0x00, 0x00, 0x00, 0x00, 0x20, 0x00, 0x20, 0x00, 0x00, 0x04};
+	uint8 web[19]={0x21, 0xFF, 0x0B, 0x4E, 0x45, 0x54, 0x53, 0x43, 0x41, 0x50, 0x45, 0x32, 0x2E, 0x30, 0x03, 0x01, 0x00, 0x00, 0x00};
+	char fname[17];
+	uint8 x, y, z;
+	uint8 tmp[1024];
+	uint8 *icon;
+	uint16 *pal;
+	file_t f;
+	GifSave *gif;
+	DreamcastFile *df;
+
+	printf(" [+] Reading %s ... ", src->getFileName());
+	if (src->readData() != 0) {
+		printf(" [-] Source file %s could not be readed.\n", src->getFileName());
+		return(-1);
+	}
+	printf("OK!\n");
+	df=src->getDCFile();
+	printFileInfo(df);
+	if (fn == NULL) {
+		sprintf(fname, "%s.GIF", df->getName());
+	} else {
+		strcpy(fname, fn);
+	}
+	printf(" [+] Writing %s ... ", fname);
+	if (!(f=fs_open(fname, "wb"))) {
+		printf("ERROR: Can't open %s!\n", fname);
+		printf(" [-] Destination file %s could not be written.\n", fname);
+		return(-2);
+	}
+	fs_write(f, hdr, 13);
+	pal=df->getIconPalette();
+	for (x=0; x<16; x++) {
+		tmp[x*3]=((pal[x] & 0x0f00) >> 8) *17;
+		tmp[x*3 + 1]=((pal[x] & 0x00f0) >> 4) *17;
+		tmp[x*3 + 2]=(pal[x] & 0x000f) *17;
+	}
+	fs_write(f, tmp, 48);
+	fs_write(f, web, 19);
+	ani[4]=df->getAnimationSpeed()*4;
+	gif=new GifSave();
+	for (z=0; z<df->getIcons(); z++) {	
+		icon=df->getIconBitmap(z);
+		fs_write(f, ani, 19);
+		for (y=0; y<32; y++) {
+			for (x=0; x<32; x+=2) {
+				tmp[y*32 + x]=(icon[y*16 + x/2] & 0xf0) >> 4;
+				tmp[y*32 + x+1]=(icon[y*16 + x/2] & 0x0f);
+			}
+		}
+		gif->LZW_Compress(4, tmp, 1024, f);
+		fs_write(f, "\0\0", 2);
+	}
+	delete gif;
+	fs_write(f, ";", 1);
+	fs_close(f);
+	printf("OK!\n");
+	return(0);
+}
+
+int main(int argc, char **argv) {
+	VirtualFile *src;
+	
+	printf("\n VMU 2 GIF Tool v1.0\n El Bucanero - www.bucanero.com.ar\n\n");
+	argc--;
+	if ((argc != 2) && (argc != 1)) {
+		printf("usage: vmu2gif <source file> [destination file]\n\n");
+		printf("about:\n This tool extracts icon images from VMU files.\n Supported source formats are .DCI and .VMI/VMS.\n\n");
+		printf("NOTE:\n Destination file name is optional.\n\n");
+		printf("examples:\n vmu2gif SONIC.VMI\n");
+		printf(" vmu2gif SHENMUE.DCI SHENICON.GIF\n");
+		return(0);
+	}
+	if (strstr(argv[1], ".DCI") || strstr(argv[1], ".dci")) {
+		src=new DCIFile(argv[1]);
+	} else if (strstr(argv[1], ".VMI") || strstr(argv[1], ".vmi")) {
+		src=new VMIFile(argv[1], "", "", "");
+	} else {
+		printf(" [-] ERROR: Invalid source file: <%s>.\n", argv[1]);
+		printf(" [-] File extension not recognized.\n");
+		printf(" [-] Valid extensions are: .VMI .vmi .DCI .dci.\n");
+		return(-1);
+	}
+	if (writeIconGif(argv[2], src) != 0) {
+		printf(" [-] Failed!\n");
+	} else {
+		printf(" [+] All done!\n");
+	}
+	delete src;
+	return(0);
+}
